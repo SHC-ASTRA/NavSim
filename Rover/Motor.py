80fc60152349f84ea5a6a5802cd546ad12b78a9e
TSlaven19 <tds0038@uah.edu>
2020-09-01 17:16:19 -0500

    Added int type allowance to Motor

--- Rover/Motor.py
+++ Rover/Motor.py
@@ -4,12 +4,13 @@ from .Part import Part
 class Motor(Part):
     # Default speed of zero
     def __init__(self, rel_x, rel_y, speed=0.0):
-        if type(speed) != float:
+        accepted_types = [int, float]
+        if type(speed) not in accepted_types:
             # Save error for end of init
             e = TypeError(
-                "Motor cannot be initialized with a speed that is not of type float."
+                "Motor cannot be initialized with a speed that is not of type int or float."
             )
-        self.speed = speed
+        self.speed = float(speed)
         super().__init__(rel_x, rel_y)
         # Raise here such that errors in passed args will hit the Part constructor first
         if "e" in locals():
