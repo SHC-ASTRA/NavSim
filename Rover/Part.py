2b115c803bd7c499568b342f725eb638e27c639d
TSlaven19 <tds0038@uah.edu>
2020-09-01 17:13:07 -0500

    Added int type allowance to Part

--- Rover/Part.py
+++ Rover/Part.py
@@ -1,11 +1,12 @@
 class Part:
     def __init__(self, rel_x, rel_y):
-        if type(rel_x) != float or type(rel_y) != float:
+        accepted_types = [int, float]
+        if type(rel_x) not in accepted_types or type(rel_y) not in accepted_types:
             raise TypeError(
-                "Part must be initialized with relative x and y position of type float."
+                "Part must be initialized with relative x and y position of type int or float."
             )
-        self.rel_x = rel_x
-        self.rel_y = rel_y
+        self.rel_x = float(rel_x)
+        self.rel_y = float(rel_y)
 
     def get_rel_x(self):
         return self.rel_x
