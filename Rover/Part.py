c0ca89c47447bdf03a7efd2cb390e92e7a881c76
TSlaven19 <tds0038@uah.edu>
2020-09-01 16:19:15 -0500

    Made error more descriptive

--- Rover/Part.py
+++ Rover/Part.py
@@ -2,7 +2,7 @@ class Part:
     def __init__(self, rel_x, rel_y):
         if type(rel_x) != float or type(rel_y) != float:
             raise TypeError(
-                "Part must be initialized with relative x and y of type float."
+                "Part must be initialized with relative x and y position of type float."
             )
         self.rel_x = rel_x
         self.rel_y = rel_y
