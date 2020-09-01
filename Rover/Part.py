0475753661adfc793c57c4a47de8faa7bacaf11f
TSlaven19 <tds0038@uah.edu>
2020-09-01 16:01:25 -0500

    Created Part class

---
+++ Rover/Part.py
@@ -0,0 +1,8 @@
+class Part:
+    def __init__(self, rel_x, rel_y):
+        if type(rel_x) != float or type(rel_y) != float:
+            raise TypeError(
+                "Part must be initialized with relative x and y of type float."
+            )
+        self.rel_x = rel_x
+        self.rel_y = rel_y
