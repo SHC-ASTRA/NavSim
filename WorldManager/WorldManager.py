0a1b712d85d0007fb8a6929f64bf157a4e8c5224
Areeb Mohammed <areebmohammed11@gmail.com>
2020-09-27 11:58:01 -0500

    Update function now keeps track of elapsed time and applies physics to the rover

--- WorldManager/WorldManager.py
+++ WorldManager/WorldManager.py
@@ -1,25 +1,36 @@
 from Environment.Tilemap import *
 from Rover.Rover import *
+from WorldManager.DummyPhysics import *
 
 
 class WorldManager:
-    def __init__(self, rover, tilemap):
+    def __init__(self, rover, time_scale=1.0):
         """Initializes a WorldManager instance.
 
         Keyword Arguments:
         rover -- an instance of the Rover class
-        tilemap -- an instance of a TileMap class
+        timescale -- how fast should the simulation run relative to real life
         """
         if type(rover) != Rover:
             raise TypeError(
-                "Must be initialized with a Rover class.\
+                "rover parameter must be a Rover class.\
                 \nSee /Rover/Rover.py"
             )
         self.rover = rover
 
-        if type(tilemap) != Tilemap:
+        if type(time_scale) not in [int, float]:
             raise TypeError(
-                "Must be initialized with a Tilemap class.\
-                \nSee /Environment/Tilemap.py"
+                "timescale parameter must be an integer or floating point number."
             )
-        self.tilemap = tilemap
+        self.timescale = time_scale
+        self.time_elapsed = 0
+
+        self.tilemap = Tilemap()
+
+    def update(self, timestep):
+        simtimestep = timestep * self.timescale
+
+        update_physics(self.rover, self.tilemap, simtimestep)
+
+        self.time_elapsed += simtimestep
+
